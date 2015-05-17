a0dc35f2fbb493e5b17790a6d9c14dd49bf7ebc6
Jesse Drelick <info@jessedrelick.com>
2015-05-16 21:33:01 -0700

    REST JWT + CORS

--- rest.js
+++ rest.js
@@ -1,10 +1,25 @@
 var config = require("./config");
 var express = require('express');
-var jwt = require('express-jwt');
+var jwt = require('jsonwebtoken');
 var rest = express();
 
-rest.use(function (req, res) {
-   jwt({secret: config.get('salt')});
+rest.use(function (req, res, next) {
+   var token = req.headers.authorization;
+   req.user_id = false;
+   try {
+      jwt.verify(token, config.get('salt'));
+      var decoded = jwt.decode(token);
+      req.user_id = decoded.user_id;
+   } catch(err) {
+      console.log(err);
+   }
+   return next();
+});
+rest.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With, Authorization");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+  next();
 });
 rest.listen(8082);
 
